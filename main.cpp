b2291e9f5a4e068233a2577d4aa3b0765a2dc9a5
zhangguangxu <15864755587@163.com>
2019-03-10 19:22:49 +0800

    张光旭作业2.16

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 0;
+    int y = 0;
+    int end1 = 0;
+    int end2 = 0;
+    int end3 = 0;
+    int end4 = 0;
+    cout <<"Enter the first integer x =";
+    cin >> x;
+    cout <<"Enter the second integer y =";
+    cin >> y;
+    end1 = x+y;
+    end2 = x-y;
+    end3 = x*y;
+    end4 = x/y;
+    cout <<"end1 ="<< end1<<endl;
+    cout <<"end2 ="<< end2<<endl;
+    cout <<"end3 ="<< end3<<endl;
+    cout <<"end4 ="<< end4<<endl;
+
+}
