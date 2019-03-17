53dba5ed17fc350a4fa13b59f67d16e108ff9a25
zhangguangxu <15864755587@163.com>
2019-03-17 19:28:29 +0800

    测四张光旭2.23

--- main.cpp
+++ main.cpp
@@ -4,38 +4,39 @@ using namespace std;
 
 int main()
 {
-    int x=0;
-    int y=0;
-    int z=0;
-    int Sum=0;
-    int Average=0;
-    int Product=0;
-    int Largest=0;
-    int Smallest=0;
-    cout <<"Enter three integers";
-    cout <<"x=";
-    cin >>x;
-    cout <<"y=";
-    cin >>y;
-    cout <<"z=";
-    cin >>z;
-    Sum =x+y+z;
-    cout <<"Sum is "<<Sum<<endl;
-    Average =(x+y+z)/3;
-    cout <<"Average is "<<Average <<endl;
-    Product =x*y*z;
-    cout <<"Product is "<<Product<<endl;
-    if (x<y)
-    Largest =y;
-    if (Largest<z)
-    Largest =z;
-    cout<<"Largest is"<<Largest <<endl;
-    Smallest =x;
-    if (Smallest<y)
-    Smallest =y;
-    if (Smallest <z)
-    Smallest =z;
-    cout <<"Smallest is"<<Smallest<<endl;
+    int a=0;
+    int b=0;
+    int c=0;
+    int d=0;
+    int e=0;
+    int largest =0;
+    int smallest =0;
+    cout <<"Enter five integers";
+    cout <<"a=";
+    cin >>a;
+    cout <<"b=";
+    cin >>b;
+    cout <<"c=";
+    cin >>c;
+    cout <<"d=";
+    cin >>d;
+    cout <<"e=";
+    cin >>e;
+    smallest=a;
+    if(smallest>b)
+    smallest=b;
+    if(smallest>c)
+    smallest=c;
+    if(smallest>d)
+    smallest=d;
+    if(smallest>e)
+    smallest=e;
+    cout <<"smallest is"<<smallest<<endl;
     return 0;
+
+
+
+
+
 
 }
