8fdcdd3c5e3449fb1107548fcc7935b3d47857ab
zhangguangxu <15864755587@163.com>
2019-03-17 19:52:59 +0800

    测四张光旭2.24

--- main.cpp
+++ main.cpp
@@ -4,49 +4,14 @@ using namespace std;
 
 int main()
 {
-    int a=0;
-    int b=0;
-    int c=0;
-    int d=0;
-    int e=0;
-    int largest =0;
-    int smallest =0;
-    cout <<"Enter five integers";
-    cout <<"a=";
-    cin >>a;
-    cout <<"b=";
-    cin >>b;
-    cout <<"c=";
-    cin >>c;
-    cout <<"d=";
-    cin >>d;
-    cout <<"e=";
-    cin >>e;
-    smallest=a;
-    if(smallest>b)
-    smallest=b;
-    if(smallest>c)
-    smallest=c;
-    if(smallest>d)
-    smallest=d;
-    if(smallest>e)
-    smallest=e;
-    cout <<"smallest is"<<smallest<<endl;
-    largest =b;
-    if (largest<a)
-    largest =a;
-    if (largest<c)
-    largest =c;
-    if (largest<d)
-    largest =d;
-    if (largest<e)
-    largest =e;
-    cout <<"largest is"<<largest<<endl;
+    int x=0;
+    cout <<"Enter one integer";
+    cout <<"x=";
+    cin >>x;
+    if (x%2==0)
+    cout <<"x is even";
+    if (x%2!=0)
+    cout <<"y is odd";
     return 0;
-
-
-
-
-
 
 }
