f314928fad751eb1e66fa73d041db6790f269619
zhangguangxu <15864755587@163.com>
2019-03-24 19:07:34 +0800

    测四张光旭

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
-#include<iostream>
-#include<string>
-using namespace std;
-
-
+#include"account.h"
 int main()
 {
-	GradeBook gradebook("C++ Programming","shibo");
-	gradebook.displayMessage();
-return 0;}
+  int n=0,m=0,l=0;
+  cout<<"请输入你的余额 ";
+  cin>>n;
+  cout<<"存款 ";
+  cin>>m;
+  cout<<"取款 ";
+  cin>>l;
+  if(l<=n)
+    l=l;
+    else
+    l=0&&cout<<"Debit amount exceeded account balance.";
+  Account account(n,m,l);
+  account.displaymessage();
+    return 0;
+}
